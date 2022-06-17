bf358a7546b9a7eafbb032c07b4f051007f24e59
Nagarjuna4085 <nagarjuna.sanem@gmail.com>
2022-06-17 23:18:43 +0530

    new commit

--- src/components/LayoutContainer.vue
+++ src/components/LayoutContainer.vue
@@ -115,7 +115,29 @@
 <!-- form -->
   <div>
     
-      
+        <el-table
+        stripe
+        border
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+      sortable
+        prop="date"
+        label="Date"
+        width="180">
+      </el-table-column>
+      <el-table-column
+      sortable
+        prop="name"
+        label="Name"
+        width="180">
+      </el-table-column>
+      <el-table-column
+      sortable
+        prop="address"
+        label="Address">
+      </el-table-column>
+    </el-table>
   </div>
 
   </div>
@@ -138,7 +160,24 @@ export default {
       Month:"",
       Year:'',
       rate1:2,
-      color:'#406fff'
+      color:'#406fff',
+          tableData: [{
+            date: '2016-05-03',
+            name: 'Tom',
+            address: 'No. 189, Grove St, Los Angeles'
+          }, {
+            date: '2016-05-02',
+            name: 'Tom',
+            address: 'No. 189, Grove St, Los Angeles'
+          }, {
+            date: '2016-05-04',
+            name: 'Tom',
+            address: 'No. 189, Grove St, Los Angeles'
+          }, {
+            date: '2016-05-01',
+            name: 'Tom',
+            address: 'No. 189, Grove St, Los Angeles'
+          }]
     };
   },
   methods: {
